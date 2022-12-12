dd1e13f34c423de33c0d058bd19b5afdfae25b7c
carlotta94c <cacaste@microsoft.com>
2022-12-12 14:25:14 +0100

    Adding Razor Page web api consuming the classifier model

---
+++ TweetsClassifierRazor/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.ML;
+using static TweetsClassifierRazor.TextClassifier;
+
+namespace TweetsClassifierRazor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly PredictionEnginePool<ModelInput, ModelOutput> _predictionEnginePool;
+
+        public IndexModel(ILogger<IndexModel> logger, PredictionEnginePool<ModelInput, ModelOutput> predictionEnginePool)
+        {
+            _logger = logger;
+            _predictionEnginePool = predictionEnginePool;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnGetAnalyzeTweet([FromQuery] string text)
+        {
+            if (String.IsNullOrEmpty(text)) return Content("Checking");
+
+            var input = new ModelInput { Cleaned_Tweets = text };
+
+            var prediction = _predictionEnginePool.Predict(input);
+
+            var isFake = Convert.ToBoolean(prediction.PredictedLabel) ? "Fake" : "Not Fake";
+
+            return Content(isFake);
+        }
+    }
+}
---
+++ TweetsClassifierRazor/wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function getClass(userInput) {
+    return fetch(`Index?handler=AnalyzeTweet&text=${userInput}`)
+        .then((response) => {
+            return response.text();
+        })
+}
+
+function updateMarker(markerPosition, isFake) {
+    $("#markerPosition").attr("style", `left:${markerPosition}%`);
+    $("#markerValue").text(isFake);
+}
+
+function updateClass() {
+
+    var userInput = $("#Message").val();
+
+    getClass(userInput)
+        .then((isFake) => {
+            switch (isFake) {
+                case "Fake":
+                    updateMarker(100.0, isFake);
+                    break;
+                case "Not Fake":
+                    updateMarker(0.0, isFake);
+                    break;
+                default:
+                    updateMarker(45.0, "Checking");
+            }
+        });
+}
+
+$("#Message").on('change input paste', updateClass)
